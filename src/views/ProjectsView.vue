ad739da1d927b07f00dbb27fd280ecfc349627d5
Rose Tovar <rose@vanica4rize.com>
2024-10-21 15:29:09 -0400

    fixed links

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="projects-container p-8">
     <div class="project-box bg-purple-100 p-6 rounded-lg shadow-md mb-6 relative z-10">
-      <h2 class="text-2xl font-bold mb-2">Summer 2023 Console To-dos Application</h2>
-      <a href="https://github.com/rvtovar/rust_todos" class="text-blue-500 hover:underline mb-2 block">github.com/rvtovar/rust_todos</a>
+      <h2 class="text-2xl font-bold mb-2">Summer 2023
+        <a href="https://github.com/rvtovar/rust_todos" class="text-blue-500">
+          Console To-Do's
+        </a>
+      </h2>
+
       <ul class="list-disc list-inside mb-4">
         <li>Developed a console-based to-do application with integration into a local PostgreSQL database for efficient task management and data persistence.</li>
         <li>Implemented seamless user interactions to enhance the overall functionality of the application.</li>
@@ -11,8 +15,12 @@
       <a href="https://github.com/rvtovar/rust_todos" target="_blank" class="inline-block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300">View Here</a>
     </div>
     <div class="project-box bg-green-100 p-6 rounded-lg shadow-md mb-6 relative z-10">
-      <h2 class="text-2xl font-bold mb-2">2022 – 2023 Quick Organics Web Application</h2>
-      <a href="https://app.transition.quickorganics.com/login" class="text-blue-500 hover:underline mb-2 block">app.transition.quickorganics.com/login</a>
+      <h2 class="text-2xl font-bold mb-2">2022 – 2023
+        <a href="https://app.transition.quickorganics.com/login" class="text-blue-400">
+          Quick Organics Web Application
+        </a>
+      </h2>
+
       <ul class="list-disc list-inside mb-4">
         <li>Developed scalable APIs with Django Rest Framework and PostgreSQL, significantly enhancing data retrieval efficiency.</li>
         <li>Conducted weekly Postgres Database migrations to ensure data integrity and optimize performance, utilizing advanced SQL queries.</li>
@@ -29,3 +37,5 @@
   margin: 0 auto;
 }
 </style>
+<script setup lang="ts">
+</script>
