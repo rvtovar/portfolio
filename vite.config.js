b9e81d38f33768dd46986f2fdd5b30b47fa559d0
Rose Tovar <rose@vanica4rize.com>
2024-10-21 14:48:20 -0400

    fixed bug

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  server:{
+    historyApiFallback: true
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
